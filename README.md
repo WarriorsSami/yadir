bece4273417c8f831448c7682a61a32744c19f5a
WarriorsSami <nicusor.barbut.v4j@student.ucv.ro>
2024-07-15 19:52:35 +0300

    docs: add rustdocs and general info in readme

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# yadir
-Yet Another Dependency Injection Registry for Rust
+# Yadir
+
+### **Yadir is yet another dependency injection registry for Rust.**
+
+---
+
+### **About**
+
+Yadir's API is heavily inspired by the [Microsoft.Extensions.DependencyInjection](https://learn.microsoft.com/en-us/dotnet/core/extensions/dependency-injection) library for .NET. It provides a simple and easy way to register and resolve dependencies in your Rust application at runtime.
+
+Its initial implementation is based on the [Registry design pattern](https://willcrichton.net/rust-api-type-patterns/registries.html), formulated by Will Crichton in its book about [Type-Driven API Design in Rust](https://willcrichton.net/rust-api-type-patterns/introduction.html).
+
+### **Usage**
+
+Add Yadir to your `Cargo.toml` file:
+```toml
+[dependencies]
+yadir = "0.1.0"
+```
+
+Create a new registry and register your dependencies, after implementing the `DIBuilder` trait for each one of them:
+```rust
+use crate::core::{DIBuilder, DIManager, DIObj};
+use async_trait::async_trait;
+use dyn_clone::{clone_trait_object, DynClone};
+
+// dyn-clone package is used to implement the Clone trait for boxed trait objects
+clone_trait_object!(Printer);
+
+trait Printer: Send + Sync + DynClone {
+    fn print(&self) -> String;
+}
+
+#[derive(Clone)]
+struct Bar;
+
+impl Printer for Bar {
+    fn print(&self) -> String {
+        "bar".to_string()
+    }
+}
+
+#[derive(Clone)]
+struct Foo {
+    printer: Box<dyn Printer>,
+}
+
+impl Foo {
+    fn new(printer: Box<dyn Printer>) -> Self {
+        Self { printer }
+    }
+
+    fn print(&self) -> String {
+        format!("foo {}", self.printer.print())
+    }
+}
+
+#[async_trait]
+impl DIBuilder for Bar {
+    type Input = ();
+    type Output = Box<dyn Printer>;
+
+    async fn build(_: Self::Input) -> Self::Output {
+        Box::new(Self)
+    }
+}
+
+#[async_trait]
+impl DIBuilder for Foo {
+    type Input = (DIObj<Box<dyn Printer>>, ());
+    type Output = Self;
+
+    async fn build((printer, _): Self::Input) -> Self::Output {
+        Self::new(printer.lock().unwrap().clone())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut manager = DIManager::default();
+
+    manager.build::<Bar>().await;
+    let foo = manager
+        .build::<Foo>()
+        .await
+        .unwrap()
+        .lock()
+        .unwrap()
+        .clone();
+
+    assert_eq!(foo.print(), "foo bar");
+}
+```
+
+### **License**
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- yadir/src/lib.rs
+++ yadir/src/lib.rs
@@ -1,3 +1,7 @@
+//! # yadir
+//!
+//! `yadir` is yet another simple dependency injection registry for Rust.
+
 pub mod core {
     use std::any::{Any, TypeId};
     use std::collections::HashMap;
@@ -5,10 +9,23 @@ pub mod core {
 
     use async_trait::async_trait;
 
+    /// A simple type map that stores values by their type.
     #[derive(Default)]
-    struct TypeMap(HashMap<TypeId, Box<dyn Any>>);
+    pub struct TypeMap(HashMap<TypeId, Box<dyn Any>>);
 
     impl TypeMap {
+        /// Inserts a value into the map with its inferred type as the key.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// use yadir::core::TypeMap;
+        ///
+        /// let mut map = TypeMap::default();
+        /// map.set(42);
+        ///
+        /// assert_eq!(map.get::<i32>(), Some(&42));
+        /// ```
         pub fn set<T>(&mut self, t: T)
         where
             T: Any + 'static,
@@ -16,6 +33,17 @@ pub mod core {
             self.0.insert(TypeId::of::<T>(), Box::new(t));
         }
 
+        /// Retrieves a value from the map by its type. Returns `None` if the value is not found.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// use yadir::core::TypeMap;
+        ///
+        /// let mut map = TypeMap::default();
+        ///
+        /// assert_eq!(map.get::<i32>(), None);
+        /// ```
         pub fn get<T>(&self) -> Option<&T>
         where
             T: Any + 'static,
@@ -25,6 +53,23 @@ pub mod core {
                 .map(|boxed| boxed.downcast_ref::<T>().unwrap())
         }
 
+        /// Retrieves a mutable reference to a value from the map by its type. Returns `None` if the value is not found.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// use yadir::core::TypeMap;
+        ///
+        /// let mut map = TypeMap::default();
+        /// map.set(42);
+        ///
+        /// assert_eq!(map.get_mut::<i32>(), Some(&mut 42));
+        ///
+        /// let mut value = map.get_mut::<i32>().unwrap();
+        /// *value = 43;
+        ///
+        /// assert_eq!(map.get::<i32>(), Some(&43));
+        /// ```
         pub fn get_mut<T>(&mut self) -> Option<&mut T>
         where
             T: Any + 'static,
@@ -34,6 +79,19 @@ pub mod core {
                 .map(|boxed| boxed.downcast_mut::<T>().unwrap())
         }
 
+        /// Checks if the map contains a value of a given type.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// use yadir::core::TypeMap;
+        ///
+        /// let mut map = TypeMap::default();
+        /// map.set(42);
+        ///
+        /// assert!(map.has::<i32>());
+        /// assert!(!map.has::<String>());
+        /// ```
         pub fn has<T>(&self) -> bool
         where
             T: Any + 'static,
@@ -42,20 +100,186 @@ pub mod core {
         }
     }
 
+    /// A trait for building dependencies.
+    ///
+    /// The `DIBuilder` trait is used to define a dependency that can be built by the dependency injection manager.
+    /// For each dependency, you need to implement the [`build`](DIBuilder::build) method that takes 
+    /// as input the [`Self::Input`](DIBuilder::Input) associated type and returns the [`Self::Output`](DIBuilder::Output) associated type.
     #[async_trait]
     pub trait DIBuilder {
-        type Input: GetInput;
-        type Output: 'static;
+        /// The input type embedding all the dependencies needed to build the current dependency.
+        /// 
+        /// The input type is used to pass dependencies to the builder. Keep in mind that the input type
+        /// must implement the [`GetInput`](GetInput) trait to be able to retrieve its own dependencies
+        /// from the dependency injection manager if any.
+        /// 
+        /// As ['GetInput'](GetInput) is using a recursive implementation via inductive traits, you can use tuples to pass multiple dependencies.
+        /// Also, you should wrap each dependency in a [`DIObj`](DIObj) to ensure thread safety and that the dependency can be easily
+        /// resolved by the dependency injection manager.
+        /// 
+        /// # Examples
+        /// 
+        /// ```
+        /// use async_trait::async_trait;
+        /// use yadir::core::{DIBuilder, DIManager, DIObj, GetInput};
+        ///
+        /// #[derive(Clone)]
+        /// struct Bar;
+        /// 
+        /// #[derive(Clone)]
+        /// struct Foo(Bar);
+        /// 
+        /// #[async_trait]
+        /// impl DIBuilder for Bar {
+        ///    type Input = ();
+        ///    type Output = Self;
+        /// 
+        ///     async fn build(_: Self::Input) -> Self::Output {
+        ///         Self
+        ///     }
+        /// }
+        /// 
+        /// #[async_trait]
+        /// impl DIBuilder for Foo {
+        ///     type Input = (DIObj<Bar>, ());
+        ///     type Output = Self;
+        ///
+        ///     async fn build((bar, _): Self::Input) -> Self::Output {
+        ///         Self(bar.lock().unwrap().clone())
+        ///     }
+        /// }
+        /// 
+        /// # #[tokio::main]
+        /// # async fn main() {
+        /// #    let mut manager = DIManager::default();
+        /// #
+        /// #    manager.build::<Bar>().await;
+        /// #    manager.build::<Foo>().await; 
+        /// #
+        /// #    assert!(manager.has::<DIObj<Bar>>());
+        /// # }
+        /// ```
+        type Input: GetInput + Clone;
+        
+        /// The output type representing the built dependency.
+        /// 
+        /// The output type is the type of the dependency that will be built by the builder after resolving all its dependencies.
+        /// Notice that the lifetime of the output type must be `'static` to ensure that the dependency injection manager does not
+        /// allow for invalid references to types to be stored in the type map.
+        type Output: 'static + Clone;
 
+        /// Builds the dependency using the input type.
+        /// 
+        /// The `build` method is used to build the dependency using the input type. The input type is used to pass all the dependencies
+        /// needed to build the current dependency. The method should return the built dependency as the output type.
+        /// 
+        /// # Examples
+        /// 
+        /// ```
+        /// use async_trait::async_trait;
+        /// use yadir::core::{DIBuilder, DIManager, DIObj, GetInput};
+        ///
+        /// #[derive(Clone)]
+        /// struct Bar;
+        /// 
+        /// #[derive(Clone)]
+        /// struct Foo(Bar);
+        /// 
+        /// #[async_trait]
+        /// impl DIBuilder for Bar {
+        ///    type Input = ();
+        ///    type Output = Self;
+        /// 
+        ///     async fn build(_: Self::Input) -> Self::Output {
+        ///         Self
+        ///     }
+        /// }
+        /// 
+        /// #[async_trait]
+        /// impl DIBuilder for Foo {
+        ///     type Input = (DIObj<Bar>, ());
+        ///     type Output = Self;
+        ///
+        ///     async fn build((bar, _): Self::Input) -> Self::Output {
+        ///         Self(bar.lock().unwrap().clone())
+        ///     }
+        /// }
+        /// 
+        /// # #[tokio::main]
+        /// # async fn main() {
+        /// #    let mut manager = DIManager::default();
+        /// #
+        /// #    manager.build::<Bar>().await;
+        /// #    manager.build::<Foo>().await; 
+        /// #
+        /// #    assert!(manager.has::<DIObj<Bar>>());
+        /// # }
+        /// ```
         async fn build(input: Self::Input) -> Self::Output;
     }
 
+    /// A wrapper type for a thread-safe reference counted mutex to handle thread-safe sharing of embedded dependencies.
     pub type DIObj<T> = Arc<Mutex<T>>;
 
+    /// A struct used to model a dependency injection manager.
+    /// 
+    /// The `DIManager` struct is used to manage the dependencies and build them using the [`build`](DIManager::build) method.
+    /// The manager uses a [`TypeMap`](TypeMap) to store the dependencies by their type.
     #[derive(Default)]
     pub struct DIManager(TypeMap);
 
     impl DIManager {
+        /// Builds a dependency using the dependency injection manager.
+        /// 
+        /// The `build` method is used to build a dependency using the dependency injection manager. The method takes a type parameter `T`
+        /// that must implement the [`DIBuilder`](DIBuilder) trait. Afterward, it returns a [`DIObj`](DIObj) that wraps the built dependency 
+        /// and stores it in the dependency injection manager.
+        /// 
+        /// The method returns `None` if the dependency could not be built.
+        /// 
+        /// # Examples
+        /// 
+        /// ```
+        /// # use async_trait::async_trait;
+        /// # use yadir::core::{DIBuilder, DIObj, GetInput};
+        /// use yadir::core::DIManager;
+        ///
+        /// # #[derive(Clone)]
+        /// # struct Bar;
+        /// # 
+        /// # #[derive(Clone)]
+        /// # struct Foo(Bar);
+        /// # 
+        /// # #[async_trait]
+        /// # impl DIBuilder for Bar {
+        /// #    type Input = ();
+        /// #    type Output = Self;
+        /// # 
+        /// #     async fn build(_: Self::Input) -> Self::Output {
+        /// #         Self
+        /// #     }
+        /// # }
+        /// # 
+        /// # #[async_trait]
+        /// # impl DIBuilder for Foo {
+        /// #     type Input = (DIObj<Bar>, ());
+        /// #     type Output = Self;
+        /// #
+        /// #     async fn build((bar, _): Self::Input) -> Self::Output {
+        /// #         Self(bar.lock().unwrap().clone())
+        /// #     }
+        /// # }
+        /// # 
+        /// #[tokio::main]
+        /// async fn main() {
+        ///     let mut manager = DIManager::default();
+        ///
+        ///     manager.build::<Bar>().await;
+        ///     manager.build::<Foo>().await; 
+        ///
+        ///     assert!(manager.has::<DIObj<Bar>>());
+        /// }
+        /// ```
         pub async fn build<T>(&mut self) -> Option<DIObj<T::Output>>
         where
             T: DIBuilder,
@@ -66,8 +290,69 @@ pub mod core {
             self.0.set::<DIObj<T::Output>>(sync_obj.clone());
             Some(sync_obj)
         }
+        
+        /// Checks if the dependency injection manager contains a dependency of a given type.
+        /// 
+        /// The `has` method is used to check if the dependency injection manager contains a dependency of a given type.
+        /// 
+        /// # Examples
+        /// 
+        /// ```
+        /// use async_trait::async_trait;
+        /// use yadir::core::{DIBuilder, DIObj, GetInput};
+        /// use yadir::core::DIManager;
+        ///
+        /// #[derive(Clone)]
+        /// struct Bar;
+        ///
+        /// # #[derive(Clone)]
+        /// # struct Foo(Bar);
+        /// # 
+        /// #[async_trait]
+        /// impl DIBuilder for Bar {
+        ///     type Input = ();
+        ///     type Output = Self;
+        /// 
+        ///      async fn build(_: Self::Input) -> Self::Output {
+        ///          Self
+        ///      }
+        /// }
+        ///  
+        /// # #[async_trait]
+        /// # impl DIBuilder for Foo {
+        /// #     type Input = (DIObj<Bar>, ());
+        /// #     type Output = Self;
+        /// #
+        /// #     async fn build((bar, _): Self::Input) -> Self::Output {
+        /// #         Self(bar.lock().unwrap().clone())
+        /// #     }
+        /// # }
+        /// # 
+        /// #[tokio::main]
+        /// async fn main() {
+        ///     let mut manager = DIManager::default();
+        ///
+        ///     manager.build::<Bar>().await;
+        /// #    manager.build::<Foo>().await; 
+        ///
+        ///     assert!(manager.has::<DIObj<Bar>>());
+        /// }
+        /// ``` 
+        pub fn has<T>(&self) -> bool
+        where
+            T: Any + 'static,
+        {
+            self.0.has::<T>()
+        }
     }
 
+    /// A trait used to retrieve dependencies from the dependency injection manager.
+    /// 
+    /// The `GetInput` trait is used to inductively resolve all the dependencies needed to build the implementer type from the dependency injection manager.
+    /// It is implemented for the following cases:
+    /// - `DIObj<T>`: to retrieve a dependency wrapped in a thread-safe reference counted mutex from the dependency injection manager (**base case**).
+    /// - `()`: to return the unit type when no dependencies are needed (**base case**).
+    /// - `(S, T)`: to retrieve multiple dependencies by recursively resolving each dependency (**inductive case**).
     pub trait GetInput: Sized {
         fn get_input(manager: &DIManager) -> Option<Self>;
     }
