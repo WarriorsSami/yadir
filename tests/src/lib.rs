cef2634ce8f107b23dac23d09354b3f503222a04
WarriorsSami <sbarbutdica@gmail.com>
2024-07-19 15:52:53 +0300

    perf(proc_macro): add support for multiple build methods from inputs
    - in-place init
    - using new method
    - using Default trait

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -8,7 +8,7 @@ mod tests {
     use yadir::{deps, let_deps};
 
     #[tokio::test]
-    async fn test_di_builder_proc_macro_all_fields_as_deps() {
+    async fn test_di_builder_proc_macro_for_all_named_fields_as_deps() {
         #[derive(Clone, DIBuilder)]
         struct Bar;
 
@@ -17,12 +17,133 @@ mod tests {
 
         #[derive(Clone, DIBuilder)]
         struct Foo {
-            #[di_build]
+            #[deps]
             bar: Bar,
-            #[di_build]
+            #[deps]
+            baz: Baz,
+        }
+
+        let mut manager = DIManager::default();
+        manager.build::<Bar>().await;
+        manager.build::<Baz>().await;
+        let foo = manager.build::<Foo>().await;
+
+        assert_some!(foo);
+    }
+
+    #[tokio::test]
+    async fn test_di_builder_proc_macro_for_all_unnamed_fields_as_deps() {
+        #[derive(Clone, DIBuilder)]
+        struct Bar;
+
+        #[derive(Clone, DIBuilder)]
+        struct Baz;
+
+        #[derive(Clone, DIBuilder)]
+        struct Foo(#[deps] Bar, #[deps] Baz);
+
+        let mut manager = DIManager::default();
+        manager.build::<Bar>().await;
+        manager.build::<Baz>().await;
+        let foo = manager.build::<Foo>().await;
+
+        assert_some!(foo);
+    }
+    
+    #[tokio::test]
+    async fn test_di_builder_proc_macro_for_all_named_fields_as_deps_with_new_build_method() {
+        #[derive(Clone, DIBuilder)]
+        struct Bar;
+
+        #[derive(Clone, DIBuilder)]
+        struct Baz;
+
+        #[derive(Clone, DIBuilder)]
+        #[build_method("new")]
+        struct Foo {
+            #[deps]
+            bar: Bar,
+            #[deps]
             baz: Baz,
         }
         
+        impl Foo {
+            fn new(bar: Bar, baz: Baz) -> Self {
+                Self { bar, baz }
+            }
+        }
+
+        let mut manager = DIManager::default();
+        manager.build::<Bar>().await;
+        manager.build::<Baz>().await;
+        let foo = manager.build::<Foo>().await;
+
+        assert_some!(foo);
+    }
+    
+    #[tokio::test]
+    async fn test_di_builder_proc_macro_for_all_unnamed_fields_as_deps_with_new_build_method() {
+        #[derive(Clone, DIBuilder)]
+        struct Bar;
+
+        #[derive(Clone, DIBuilder)]
+        struct Baz;
+
+        #[derive(Clone, DIBuilder)]
+        #[build_method("new")]
+        struct Foo(#[deps] Bar, #[deps] Baz);
+        
+        impl Foo {
+            fn new(bar: Bar, baz: Baz) -> Self {
+                Self(bar, baz)
+            }
+        }
+
+        let mut manager = DIManager::default();
+        manager.build::<Bar>().await;
+        manager.build::<Baz>().await;
+        let foo = manager.build::<Foo>().await;
+
+        assert_some!(foo);
+    }
+    
+    #[tokio::test]
+    async fn test_di_builder_proc_macro_for_all_named_fields_as_deps_with_default_build_method() {
+        #[derive(Default, Clone, DIBuilder)]
+        struct Bar;
+
+        #[derive(Default, Clone, DIBuilder)]
+        struct Baz;
+
+        #[derive(Default, Clone, DIBuilder)]
+        #[build_method("default")]
+        struct Foo {
+            #[deps]
+            bar: Bar,
+            #[deps]
+            baz: Baz,
+        }
+
+        let mut manager = DIManager::default();
+        manager.build::<Bar>().await;
+        manager.build::<Baz>().await;
+        let foo = manager.build::<Foo>().await;
+
+        assert_some!(foo);
+    }
+    
+    #[tokio::test]
+    async fn test_di_builder_proc_macro_for_all_unnamed_fields_as_deps_with_default_build_method() {
+        #[derive(Default, Clone, DIBuilder)]
+        struct Bar;
+
+        #[derive(Default, Clone, DIBuilder)]
+        struct Baz;
+
+        #[derive(Default, Clone, DIBuilder)]
+        #[build_method("default")]
+        struct Foo(#[deps] Bar, #[deps] Baz);
+
         let mut manager = DIManager::default();
         manager.build::<Bar>().await;
         manager.build::<Baz>().await;
--- yadir_derive/src/lib.rs
+++ yadir_derive/src/lib.rs
@@ -1,16 +1,54 @@
-#[proc_macro_derive(DIBuilder, attributes(di_build))]
+enum BuildMethod {
+    None,
+    New,
+    Default,
+}
+
+impl From<String> for BuildMethod {
+    fn from(s: String) -> Self {
+        match s.as_str() {
+            "new" => Self::New,
+            "default" => Self::Default,
+            _ => Self::None,
+        }
+    }
+}
+
+#[proc_macro_derive(DIBuilder, attributes(deps, build_method))]
 pub fn derive_di_builder(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as syn::ItemStruct);
+    
+    // get the value of the #[build_method] attribute
+    let build_method = input
+        .attrs
+        .iter()
+        .find(|attr| attr.path().is_ident("build_method"))
+        .map(|attr| {
+            attr.parse_args::<syn::LitStr>()
+                .expect("expected a string literal")
+                .value()
+        })
+        .map(BuildMethod::from)
+        .unwrap_or(BuildMethod::None);
 
     // get the types of all fields which are annotated with #[di_build]
     let field_types = match &input.fields {
         syn::Fields::Named(fields) => fields
             .named
             .iter()
-            .map(|field| {
+            .filter_map(|field| {
                 let ty = &field.ty;
-                quote::quote! {
-                    #ty
+
+                if field
+                    .attrs
+                    .iter()
+                    .any(|attr| attr.path().is_ident("deps"))
+                {
+                    Some(quote::quote! {
+                        #ty
+                    })
+                } else {
+                    None
                 }
             })
             .collect::<Vec<_>>(),
@@ -18,74 +56,152 @@ pub fn derive_di_builder(input: proc_macro::TokenStream) -> proc_macro::TokenStr
             .unnamed
             .iter()
             .enumerate()
-            .map(|(_, field)| {
+            .filter_map(|(_, field)| {
                 let ty = &field.ty;
-                quote::quote! {
-                    #ty
+
+                if field
+                    .attrs
+                    .iter()
+                    .any(|attr| attr.path().is_ident("deps"))
+                {
+                    Some(quote::quote! {
+                        #ty
+                    })
+                } else {
+                    None
                 }
             })
             .collect::<Vec<_>>(),
         syn::Fields::Unit => vec![],
     };
 
-    let field_idents = match &input.fields {
+    let named_field_idents = match &input.fields {
         syn::Fields::Named(fields) => fields
             .named
             .iter()
-            .map(|field| {
+            .filter_map(|field| {
                 let ident = field.ident.as_ref().unwrap();
-                quote::quote! {
-                    #ident
+
+                if field
+                    .attrs
+                    .iter()
+                    .any(|attr| attr.path().is_ident("deps"))
+                {
+                    Some(ident)
+                } else {
+                    None
                 }
             })
             .collect::<Vec<_>>(),
+        _ => vec![],
+    };
+
+    let unnamed_field_idents = match &input.fields {
         syn::Fields::Unnamed(fields) => fields
             .unnamed
             .iter()
             .enumerate()
-            .map(|(i, _)| {
-                let ident = syn::Ident::new(&format!("field{}", i), proc_macro2::Span::call_site());
-                quote::quote! {
-                    #ident
+            .filter_map(|(i, field)| {
+                let ident =
+                    syn::Ident::new(&format!("field_{}", i), proc_macro2::Span::call_site());
+
+                if field
+                    .attrs
+                    .iter()
+                    .any(|attr| attr.path().is_ident("deps"))
+                {
+                    Some(ident)
+                } else {
+                    None
                 }
             })
             .collect::<Vec<_>>(),
-        syn::Fields::Unit => vec![],
+        _ => vec![],
+    };
+    
+    // construct the instantiation of the input struct based on the #[deps] fields and the #[build_method] attribute
+    let build_method = match (build_method, named_field_idents.is_empty(), unnamed_field_idents.is_empty()) {
+        (BuildMethod::None, true, true) => quote::quote! {
+            Self
+        },
+        (BuildMethod::None, false, true) => quote::quote! {
+            let_deps!(#(#named_field_idents),* <- input);
+            
+            Self {
+                #(
+                    #named_field_idents
+                ),*
+            }
+        },
+        (BuildMethod::None, true, false) => quote::quote! {
+            let_deps!(#(#unnamed_field_idents),* <- input);
+            
+            Self(
+                #(
+                    #unnamed_field_idents
+                ),*
+            )
+        },
+        (BuildMethod::New, true, true) => quote::quote! {
+            Self::new()
+        },
+        (BuildMethod::New, false, true) => quote::quote! {
+            let_deps!(#(#named_field_idents),* <- input);
+            
+            Self::new(
+                #(
+                    #named_field_idents
+                ),*
+            )
+        },
+        (BuildMethod::New, true, false) => quote::quote! {
+            let_deps!(#(#unnamed_field_idents),* <- input);
+            
+            Self::new(
+                #(
+                    #unnamed_field_idents
+                ),*
+            )
+        },
+        (BuildMethod::Default, false, true) => quote::quote! {
+            Self::default()
+        },
+        (BuildMethod::Default, true, false) => quote::quote! {
+            Self::default()
+        },
+        _ => panic!("Cannot mix named and unnamed fields with #[di_build]"),
     };
 
     // get the name of the input struct
     let input = input.ident;
 
-    let output = if field_idents.is_empty() {
-        quote::quote! {
+    let output = match (
+        named_field_idents.is_empty(),
+        unnamed_field_idents.is_empty(),
+    ) {
+        (true, true) => quote::quote! {
             #[async_trait]
             impl DIBuilder for #input {
                 type Input = deps!();
                 type Output = Self;
 
                 async fn build(_: Self::Input) -> Self::Output {
-                    Self
+                    #build_method
                 }
             }
-        }
-    } else {
-        quote::quote! {
+        },
+        (false, true) | (true, false) => quote::quote! {
             #[async_trait]
             impl DIBuilder for #input {
                 type Input = deps!(#(#field_types),*);
                 type Output = Self;
 
                 async fn build(input: Self::Input) -> Self::Output {
-                    let_deps!(#(#field_idents),* <- input);
-
-                    Self {
-                        #(
-                            #field_idents
-                        ),*
-                    }
+                    #build_method
                 }
             }
-        }
+        },
+        _ => panic!("Cannot mix named and unnamed fields with #[di_build]"),
     };
 
     output.into()
